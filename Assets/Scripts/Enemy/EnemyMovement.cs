9017e65fa635b1b42eba4375d572f2024b8c7646
Jordan Truong <jordantruong26@gmail.com>
2026-05-05 11:06:26 -0700

    create enemy movement

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    public float moveSpeed;
+
+    void Start()
+    {
+        player = FindAnyObjectByType<PlayerMovement>().transform;
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+    }
+}
