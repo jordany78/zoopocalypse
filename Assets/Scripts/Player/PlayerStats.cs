e6fb7280bb79ddcf8d352ae40c8e782c0d8f0937
Jordan Truong <jordantruong26@gmail.com>
2026-05-06 13:15:05 -0700

    advanced level system

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
@@ -10,6 +12,26 @@ public class PlayerStats : MonoBehaviour
     float currentMight;
     float currentProjectileSpeed;
 
+    // Experience and level of the player
+    [Header("Experience/Level")]
+    public int experience = 0;
+    public int level = 1;
+    public int experienceCap;
+
+    [System.Serializable]
+    public class LevelRange
+    {
+        public int startLevel;
+        public int endLevel;
+        public int experienceCapIncrease;
+        public LevelRange(int start, int end)
+        {
+            startLevel = start;
+            endLevel = end;
+        }
+    }
+
+    public List<LevelRange> levelRanges;
     void Awake()
     {
         currentHealth = characterData.MaxHealth;
@@ -18,4 +40,36 @@ public class PlayerStats : MonoBehaviour
         currentMight = characterData.Might;
         currentProjectileSpeed = characterData.ProjectileSpeed;
     }
+
+    void Start()
+    {
+        experienceCap = levelRanges[0].experienceCapIncrease;
+    }
+
+    public void IncreaseExperience(int amount)
+    {
+        experience += amount;
+        LevelUpChecker();
+    }
+
+    void LevelUpChecker()
+    {
+        if(experience >= experienceCap)
+        {
+            level++;
+            experience -= experienceCap;
+
+            int experienceCapIncrease = 0;
+            foreach (LevelRange range in levelRanges)
+            {
+                if(level >= range.startLevel && level <= range.endLevel)
+                {
+                    experienceCapIncrease = range.experienceCapIncrease;
+                    break;
+                }
+            }
+            experienceCap += experienceCapIncrease;
+        }
+    }
+
 }
